877ce3f14b8fa71c3ce6585d0c5604de18e2b7b9
hellmir <hellmir610@gmail.com>
2025-12-23 17:02:59 +0900

    [#1][#2] feat: 로깅 인터셉터 설정 클래스 추가 (#12)

---
+++ src/main/java/com/oponiti/shopreward/common/config/InterceptorConfig.java
@@ -0,0 +1,14 @@
+package com.oponiti.shopreward.common.config;
+
+import com.oponiti.shopreward.common.interception.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
+    }
+}
