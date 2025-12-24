106443dfa075f58f3fcd4214c76b41bed9fe697d
hellmir <hellmir610@gmail.com>
2025-12-24 15:47:38 +0900

    [#1][#8] feat: 모니터링 시스템 빌드를 위해 시큐리티 관련 의존성 임시 제외 (#25)

--- src/main/java/com/oponiti/shopreward/common/config/AccessPermissionRegistryConfig.java
+++ src/main/java/com/oponiti/shopreward/common/config/AccessPermissionRegistryConfig.java
@@ -1,14 +1,14 @@
 package com.oponiti.shopreward.common.config;
 
-import com.oponiti.shopreward.domain.user.authentication.accesspermission.AccessPermissionRegistry;
-import com.oponiti.shopreward.domain.user.authentication.accesspermission.AccessPermissionRegistryFactory;
+//import com.oponiti.shopreward.domain.user.authentication.accesspermission.AccessPermissionRegistry;
+//import com.oponiti.shopreward.domain.user.authentication.accesspermission.AccessPermissionRegistryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AccessPermissionRegistryConfig {
-    @Bean
-    public AccessPermissionRegistry accessPermissionRegistry(AccessPermissionRegistryFactory accessPermissionRegistryFactory) {
-        return accessPermissionRegistryFactory.createAccessPermissionRegistry();
-    }
+//    @Bean
+//    public AccessPermissionRegistry accessPermissionRegistry(AccessPermissionRegistryFactory accessPermissionRegistryFactory) {
+//        return accessPermissionRegistryFactory.createAccessPermissionRegistry();
+//    }
 }
