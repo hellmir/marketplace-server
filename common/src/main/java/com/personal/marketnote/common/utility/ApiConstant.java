2cecbad21d1e36d3d0f790bbd7fdae03cb3caab1
hellmir <hellmir610@gmail.com>
2025-12-30 22:52:10 +0900

    [#128][#131] feat: 상품 등록 시 관리자 또는 판매자 권한 검증 및 인가 로직 추가 (#132)

--- common/src/main/java/com/personal/marketnote/common/utility/ApiConstant.java
+++ common/src/main/java/com/personal/marketnote/common/utility/ApiConstant.java
@@ -2,6 +2,7 @@ package com.personal.marketnote.common.utility;
 
 public class ApiConstant {
     public static final String USER_ID_KEY = "userId";
-    public static final String ADMIN_POINTCUT = "isAuthenticated() and hasRole('ROLE_ADMIN')";
     public static final String DEFAULT_PAGE_NUMBER = "1";
+    public static final String ADMIN_POINTCUT = "isAuthenticated() and hasRole('ROLE_ADMIN')";
+    public static final String ADMIN_OR_SELLER_POINTCUT = "isAuthenticated() and hasRole('ROLE_ADMIN') or hasRole('ROLE_SELLER')";
 }
--- product-service/product-adapters/src/main/java/com/personal/marketnote/product/adapter/in/client/product/controller/ProductController.java
+++ product-service/product-adapters/src/main/java/com/personal/marketnote/product/adapter/in/client/product/controller/ProductController.java
@@ -13,18 +13,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.personal.marketnote.common.domain.exception.ExceptionCode.DEFAULT_SUCCESS_CODE;
+import static com.personal.marketnote.common.utility.ApiConstant.ADMIN_OR_SELLER_POINTCUT;
 
 @RestController
 @RequestMapping("/api/v1/products")
 @Tag(name = "상품 API", description = "상품 관련 API")
 @RequiredArgsConstructor
 @Slf4j
+@PreAuthorize(ADMIN_OR_SELLER_POINTCUT)
 public class ProductController {
     private final RegisterProductUseCase registerProductUseCase;
 
