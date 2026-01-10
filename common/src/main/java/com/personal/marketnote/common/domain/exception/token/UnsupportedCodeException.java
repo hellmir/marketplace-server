acfb0058a7ef35d22f38ac55b0763fb39eca78f7
hellmir <hellmir610@gmail.com>
2026-01-10 12:04:52 +0900

    [#300][#303] feat: 회원 리뷰 등록 비즈니스 로직 리팩터링 (#315)
    - [x] 평점 소수값 변환 로직 리팩터링
    - [x] 리뷰 작성자 이름 마스킹 로직 리팩터링

--- common/src/main/java/com/personal/marketnote/common/domain/exception/token/UnsupportedCodeException.java
+++ common/src/main/java/com/personal/marketnote/common/domain/exception/token/UnsupportedCodeException.java
@@ -1,10 +1,5 @@
 package com.personal.marketnote.common.domain.exception.token;
 
-/**
- * Authorization Server에서 인가코드를 처리할 수 없을 때 발생하는 예외.
- *
- * @author 성효빈
- */
 public class UnsupportedCodeException extends TokenException {
     public UnsupportedCodeException(String message) {
         super(message);
---
+++ common/src/main/java/com/personal/marketnote/common/utility/CharacterConstant.java
@@ -0,0 +1,5 @@
+package com.personal.marketnote.common.utility;
+
+public class CharacterConstant {
+    public static final String WILD_CARD = "*";
+}
