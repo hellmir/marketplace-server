bfead67145f8d4c1bf94d559c4b4be759d449842
hellmir <hellmir610@gmail.com>
2026-01-19 20:12:03 +0900

    [#387][#442] feat: 회원 가입 시 포인트 도메인 생성 API 호출 로직 추가 (#443)
    - [x] userId
    - [x] userKey

--- user-service/user-application/src/main/java/com/personal/marketnote/user/port/out/reward/ModifyUserPointPort.java
+++ user-service/user-application/src/main/java/com/personal/marketnote/user/port/out/reward/ModifyUserPointPort.java
@@ -2,6 +2,14 @@ package com.personal.marketnote.user.port.out.reward;
 
 public interface ModifyUserPointPort {
     /**
+     * 회원 포인트 도메인 생성
+     *
+     * @param userId  회원 ID
+     * @param userKey 회원 고유 키
+     */
+    void registerUserPoint(Long userId, String userKey);
+
+    /**
      * 추천 포인트 적립
      *
      * @param referrerUserId 추천한 회원 ID
